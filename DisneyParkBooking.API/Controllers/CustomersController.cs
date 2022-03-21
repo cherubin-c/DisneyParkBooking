7d080281c5715c2ead53273d29adda921394fab7
Cherubin Chatta <cherubinchatta@gmail.com>
2022-03-21 17:20:12 +0530

    First commit

---
+++ DisneyParkBooking.API/Controllers/CustomersController.cs
@@ -0,0 +1,35 @@
+﻿using DisneyParkBooking.Data;
+using DisneyParkBooking.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DisneyParkBooking.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CustomersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+
+        }
+        [HttpPost]
+        public async Task<Customer> AddCustAsync(Customer customer)
+        {
+            _context.Add(customer);
+            await _context.SaveChangesAsync();
+            return customer;
+        }
+    }
+}
+
---
+++ DisneyParkBooking.API/Controllers/HotelsController.cs
@@ -0,0 +1,33 @@
+﻿using DisneyParkBooking.Data;
+using DisneyParkBooking.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DisneyParkBooking.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public HotelsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<Hotel> GetHotels()
+        {
+            return _context.Hotels.ToList();
+        }
+        [HttpPost]
+        public async Task<Hotel> AddCustAsync(Hotel hotel)
+        {
+            _context.Add(hotel);
+            await _context.SaveChangesAsync();
+            return hotel;
+        }
+    }
+}
